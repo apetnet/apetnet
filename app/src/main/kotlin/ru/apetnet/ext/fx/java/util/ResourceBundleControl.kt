dda9c58f7e331649009a3a8f35c2dc6b1f7bb46f
aptnt <aalog@rambler.ru>
2022-04-24 21:16:27 +0300

    Fix locale

--- app/src/main/kotlin/ru/apetnet/ext/fx/java/util/ResourceBundleControl.kt
+++ app/src/main/kotlin/ru/apetnet/ext/fx/java/util/ResourceBundleControl.kt
@@ -6,8 +6,12 @@ import java.util.*
 class ResourceBundleControl : ResourceBundle.Control() {
     override fun getCandidateLocales(baseName: String?, locale: Locale?): MutableList<Locale> {
         return mutableListOf(
-            Locale("ru"),
-            Locale("en")
+            if (locale != null && locale.language.contains("ru", ignoreCase = true)) {
+                Locale("ru")
+            }
+            else {
+                Locale("en")
+            }
         )
     }
 }
